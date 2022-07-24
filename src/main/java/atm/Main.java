7426bacd09e21dafeee5928ef68d7ebec26ddf29
Mint Nichapat <nichapat.phe@ku.th>
2022-07-24 15:15:41 +0700

    first commit

---
+++ src/main/java/atm/Main.java
@@ -0,0 +1,20 @@
+package atm;
+
+import javafx.application.Application;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+//        Bank bankXYZ = new Bank("XYZ bank", new DataSourceDB());
+//        Bank bankABC = new Bank("ABC bank", new DataSourceFile("customersABC.txt"));
+//        ATM atm = new ATM(bankABC);
+
+        ApplicationContext context
+                = new ClassPathXmlApplicationContext("bean.xml");
+
+        AtmUI atmUI = context.getBean(AtmUI.class);
+        atmUI.run();
+    }
+}
